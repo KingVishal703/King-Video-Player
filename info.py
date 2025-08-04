c81783daacb8d36b9447fd000f9cf442c84ca52f
KingVishal703 <vishal21wankhede@gmail.com>
2025-08-04 08:39:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,33 +1,35 @@
 import re
 from os import environ
 
+class Config:
+
 # Bot Session Name
 SESSION = environ.get('SESSION', 'TechVJBot')
 
 # Your Telegram Account Api Id And Api Hash
-API_ID = int(environ.get('API_ID', '28277662'))
-API_HASH = environ.get('API_HASH', '367b38674199ccc72629d43f7c073662')
-
-# Bot Token, This Is Main Bot
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(os.getenv("API_ID", "12345"))
+    API_HASH = os.getenv("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 
 # Admin Telegram Account Id For Withdraw Notification Or Anything Else
-ADMIN = int(environ.get('ADMIN', '1029462448'))
-
+ADMIN_ID = [int(i) for i in os.getenv("ADMIN_ID", "5654093580").split()]
 # Back Up Bot Token For Fetching Message When Floodwait Comes
 BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "")
 
 # Log Channel, In This Channel Your All File Stored.
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002836031835'))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001234567890"))
 
 # Mongodb Database For User Link Click Count Etc Data Store.
-MONGODB_URI = environ.get("MONGODB_URI", "")
+MONGODB_URI = os.getenv("MONGODB_URI", "")
+    DATABASE_NAME = "videobot"
 
 # Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
 STREAM_URL = environ.get("STREAM_URL", "https://bottom-abbey-bjhchfguv-a18f9cc8.koyeb.app/")
 
 # This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
-LINK_URL = environ.get("LINK_URL", "https://www.aklink.site/2025/07/aklinks.html")
+DOMAIN = os.getenv("DOMAIN", "https://short.domain")
+
+SHORTENER_API = os.getenv("SHORTENER_API", "api_key_here")
 
 # Others, Not Usefull
 PORT = environ.get("PORT", "8080")
