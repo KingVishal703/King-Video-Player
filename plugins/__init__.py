f2af95440953dd10d596dca00babbb251698efe1
KingVishal703 <vishal21wankhede@gmail.com>
2025-08-04 09:52:37 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,10 +2,16 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+"""
+This module initializes the aiohttp web server
+used for file streaming or API endpoints.
+"""
+
 from aiohttp import web
 from .route import routes
 
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
+    # Max client request size = ~30 MB
+    web_app = web.Application(client_max_size=30_000_000)
     web_app.add_routes(routes)
     return web_app
